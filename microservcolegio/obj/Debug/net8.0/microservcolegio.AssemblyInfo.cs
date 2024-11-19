b9df966feac19ee64a9b581406b3d994c82e8d45
Walter Silvestre Coan <walter.coan@gmail.com>
2024-11-19 17:11:32 +0000

    dapr

--- microservcolegio/obj/Debug/net8.0/microservcolegio.AssemblyInfo.cs
+++ microservcolegio/obj/Debug/net8.0/microservcolegio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("microservcolegio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3adec880a74ae2775e95be880018d530f948a4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad7fdad216c328709c6285ed0b66697bcc1b7bfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("microservcolegio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("microservcolegio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
